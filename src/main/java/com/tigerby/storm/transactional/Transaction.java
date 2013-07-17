bf6affe960985652190ee808d6de5073332df4fc
Kim Bryan <bongyeonkim@gmail.com>
2013-07-17 11:31:01 +0900

    add transaction interface

---
+++ src/main/java/com/tigerby/storm/transactional/Transaction.java
@@ -0,0 +1,10 @@
+package com.tigerby.storm.transactional;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author <a href="mailto:bongyeonkim@gmail.com">Bryan Kim</a>
+ * @version 1.0
+ */
+public interface Transaction {
+}
